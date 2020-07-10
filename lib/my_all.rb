8b03d524f57e5ab933ab9bb6a0084abfe7bf3234
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-10 19:57:51 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,7 +7,7 @@ while i < collection.length
   block_return_values << yield(collection[i])
   i += 1
 end
-if block_return_values.incldude>(false)
+if block_return_values.include?>(false)
   false
 else
   true
